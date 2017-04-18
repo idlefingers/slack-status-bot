aa483965b2a8e3a498354aea6c76668d906df16d
Damien <damien@izettle.com>
2017-04-18 21:21:58 +0200

    remember new status

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const token = process.env.SLACK_TOKEN
 
 const statuses = {}
 
-slack.users.list({ token }, (err, data) => {
-  data.members.map(member => (
-    statuses[member.id] = {
-      status_text: member.profile.status_text,
-      status_emoji: member.profile.status_emoji
-    }
+const setMemberStatus = (member, statusText, statusEmoji) => {
+  statuses[member.id] = {
+    statusText,
+    statusEmoji
+  }
+}
+
+const postMessage = (user, statusText, statusEmoji) => {
+  const text = `@${user.name} changed status to: ${statusEmoji} ${statusText}`
+  slack.chat.postMessage({ token, channel: "#status", text }, (err, data) => (
+    console.log(err, data)
   ))
+}
+
+slack.users.list({ token }, (err, data) => {
+  data.members.map(member =>
+    setMemberStatus(member, member.profile.status_text, member.profile.status_emoji)
+  )
 })
 
 bot.user_change(message => {
   const oldStatus = statuses[message.user.id]
-  const { status_text, status_emoji } = message.user.profile
-  if (status_text !== oldStatus.status_text || status_emoji !== oldStatus.status_emoji) {
-    const text = `@${message.user.name} changed status to: ${status_emoji} ${status_text}`
-    slack.chat.postMessage({ token, channel: "#status", text }, (err, data) => (
-      console.log(err, data)
-    ))
+  const statusText = message.user.profile.status_text
+  const statusEmoji = message.user.profile.status_emoji
+
+  if (statusText !== oldStatus.statusText || statusEmoji !== oldStatus.statusEmoji) {
+    setMemberStatus(message.user, statusText, statusEmoji)
+    postMessage(message.user, statusText, statusEmoji)
   }
 })
 
